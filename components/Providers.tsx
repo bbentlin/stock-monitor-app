9d3892b5c4c725bb2696de704245a217eedf373c
Ben Bentlin <135572747+bbentlin@users.noreply.github.com>
2026-01-28 19:25:01 -0600

    feat: Wrap children in ToastProvider for toast notifications support

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,13 +3,16 @@
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { WebSocketProvider } from "@/lib/context/WebSocketContext";
+import { ToastProvider } from "./Toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <WebSocketProvider>
-          {children}
+          <ToastProvider>
+            {children}
+          </ToastProvider>
         </WebSocketProvider>
       </ThemeProvider>
     </SessionProvider>
